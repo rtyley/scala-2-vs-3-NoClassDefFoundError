86b1c1c2dddc31cc76777422627518458e9bd7f8
Roberto Tyley <roberto.tyley@guardian.co.uk>
2021-05-16 12:42:14 +0100

    Scala 3.0.0 passes if `getClass` is not called
    It seems to have some bearing on the `java.lang.NoClassDefFoundError`

--- src/main/scala/com/madgag/example/package.scala
+++ src/main/scala/com/madgag/example/package.scala
@@ -7,7 +7,7 @@ import java.nio.file.Files.createTempDirectory
 
 package object example { // Not using a package object allows Scala 3.0.0 to succeed
   def useCompressUtils(): Unit = {
-    getClass // removing this allows Scala 3.0.0 to succeed.
+    // getClass // removing this allows Scala 3.0.0 to succeed.
 
     // Fails under Scala 3.0.0 with "java.lang.NoClassDefFoundError: com/madgag/compress/CompressUtil$"
     unzip(new ByteArrayInputStream(Array.emptyByteArray), createTempDirectory("example").toFile)
